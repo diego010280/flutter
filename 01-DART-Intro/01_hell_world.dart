ab4c4ed6c1d910270cacf35612559896c18d3d7a
Diego Rios <armando010280rios@gmail.com>
2024-01-02 11:22:57 -0300

    nuevo

---
+++ 01-DART-Intro/01_hell_world.dart
@@ -0,0 +1,12 @@
+void main(){
+  
+  //var myname = 'Diego';
+  // late final myname;
+  //const myname='Diego';
+  String myname = 'Diego';
+  
+  print('Hola $myname');
+  print('Hola ${ myname.toUpperCase()}');
+  print('Hola ${1 + 1}');
+  
+}
